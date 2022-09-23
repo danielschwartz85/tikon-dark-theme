fbb155a0f8df4dcf1a28531fa187e89aa7174fca
Daniel Schwartz <danielsch24@gmail.com>
2022-09-23 21:26:27 +0300

    removed v bug

--- original-scroll-indicator.js
+++ original-scroll-indicator.js
@@ -23,3 +23,6 @@ document.onscroll = function () {
 window.onresize = function () {
     initiateHeights();
 }
+
+// Minified:
+//    const body=document.body,html=document.documentElement;let height=0,h=0;const initiateHeights=function(){height=Math.max(body.scrollHeight,body.offsetHeight,html.clientHeight,html.scrollHeight,html.offsetHeight),h=window.innerHeight||document.documentElement.clientHeight||document.body.clientHeight,console.log("heights were initialised:",height,h)};initiateHeights();const resize=function(t){let e=Math.max(document.body.scrollTop,document.documentElement.scrollTop);height>0?t[0].style.width=e/(height-h)*100+"%":t.style.width="0%"};document.onscroll=function(){resize(document.getElementsByClassName("indicator"))},window.onresize=function(){initiateHeights()};
