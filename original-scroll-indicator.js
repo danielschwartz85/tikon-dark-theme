2bd8197cdb5010727f7a714e5354dcbc9b7e8f35
Daniel Schwartz <danielsch24@gmail.com>
2022-09-23 21:15:37 +0300

    original scroll minifer script

---
+++ original-scroll-indicator.js
@@ -0,0 +1,25 @@
+
+// Non minified:
+
+const body = document.body;
+const html = document.documentElement;
+let height = 0;
+let h = 0;
+
+const initiateHeights = function () {
+    height = Math.max(body.scrollHeight, body.offsetHeight, html.clientHeight, html.scrollHeight, html.offsetHeight);
+    h = window.innerHeight || document.documentElement.clientHeight || document.body.clientHeight;
+    console.log("heights were initialised:", height, h);
+}
+initiateHeights();
+
+const resize = function (e) {
+    const scrolled = Math.max(document.body.scrollTop, document.documentElement.scrollTop);
+    height > 0 ? e[0].style.width = scrolled / (height - h) * 100 + "%" : e.style.width = 0 + "%";
+}
+document.onscroll = function () {
+    resize(document.getElementsByClassName("indicator"));
+};
+window.onresize = function () {
+    initiateHeights();
+}
